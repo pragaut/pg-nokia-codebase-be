a2924ec487c9460c12f1877694832fb37c88869d
Pardeep Bhardwaj <pardeepbhardwaj10@mail.com>
2021-12-25 12:12:26 +0530

    pb changes

--- controllers/nokia/tmc.working.js
+++ controllers/nokia/tmc.working.js
@@ -38,15 +38,15 @@ const saveTMCDeviceBetteryStatusDetailsP = (req, res) => {
             let result = undefined;
             let id = dal.uuid(db.deviceBatteryDetails.name);
 
-            db.sequelize.query('call asp_nk_save_device_battery_status_details(:device_battery_status_detail_id,:mac_address,:main_device_battery,:child1_device_battery,:child2_device_battery,:child3_device_battery,:created_by)', {
+            db.sequelize.query('call asp_nk_save_device_battery_status_details(:p_device_battery_status_detail_id,:p_mac_address,:main_device_battery,:p_child1_device_battery,:p_child2_device_battery,:p_child3_device_battery,:created_by)', {
                 replacements: {
-                    device_battery_status_detail_id: id,
-                    mac_address: req.macAddress ? req.macAddress : '',
-                    main_device_battery: req.mainDeviceBattery ? req.mainDeviceBattery : '',
-                    child1_device_battery: req.child1DeviceBattery ? req.child1DeviceBattery : '',
-                    child2_device_battery: req.child2DeviceBattery ? req.child2DeviceBattery : '',
-                    child3_device_battery: req.child3DeviceBattery ? req.child3DeviceBattery : '',
-                    created_by: req.useId ? req.useId : '',
+                    p_device_battery_status_detail_id: id,
+                    p_mac_address: req.macAddress ? req.macAddress : '',
+                    p_main_device_battery: req.mainDeviceBattery ? req.mainDeviceBattery : '',
+                    p_child1_device_battery: req.child1DeviceBattery ? req.child1DeviceBattery : '',
+                    p_child2_device_battery: req.child2DeviceBattery ? req.child2DeviceBattery : '',
+                    p_child3_device_battery: req.child3DeviceBattery ? req.child3DeviceBattery : '',
+                    p_created_by: req.useId ? req.useId : '',
                 }
             }).then(results => {
                 result = results;
